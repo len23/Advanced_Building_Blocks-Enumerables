7a7057cfffff2e36f7c2df29b06e85c45fe1dde8
lenin <lenin.montalvo.23@hotmail.com>
2020-09-22 12:44:41 -0500

    Fix some linters errors

--- app.rb
+++ app.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 module Enumerable
   def my_each
     ary = self
@@ -18,9 +16,7 @@ module Enumerable
   def my_select
     new_arr = []
     self.my_each do |element|
-      if yield(element)
-        new_arr << element
-      end
+      if yield(element) then new_arr << element
     end
     new_arr
   end
@@ -39,7 +35,6 @@ module Enumerable
   def my_any?
     boolean = false
     self.my_each do |element|
-      binding.pry
       if yield(element)
         boolean = true
         break
@@ -50,7 +45,7 @@ module Enumerable
 
   def my_none?
     boolean = true
-    self.my_each do |element| 
+    self.my_each do |element|
       if yield(element)
         boolean = false
         break
@@ -61,10 +56,8 @@ module Enumerable
 
   def my_count
     counter = 0
-    self.my_each do |element| 
-      if yield(element)
-        counter += 1
-      end
+    self.my_each do |element|
+      if yield(element) then counter += 1
     end
     counter
   end
@@ -72,14 +65,14 @@ module Enumerable
   def my_map
     new_arr = []
     self.my_each do |element|
-        new_arr << yield(element)
+      new_arr << yield(element)
     end
     new_arr
   end
 
   def my_inject(initial = nil)
     accum = 0
-    if initial === nil
+    if initial == nil
       initial = self[0]
       self.my_each_with_index do |element, index|
         if index + 1 == self.length
@@ -97,14 +90,17 @@ module Enumerable
       self.my_each do |element|
         accum = yield(accum, element)
     end
-  end
     accum
   end
 
-  def my_map_proc(p1)
+  def my_map_proc(p1=nil)
     new_arr = []
     self.my_each do |element|
-      new_arr << p1.call(element)
+     if p1 == nil
+        new_arr << yield(element)
+     else
+        new_arr << p1.call(element)
+     end
     end
     new_arr
   end
@@ -113,9 +109,3 @@ end
 def multiply_els(array)
   array.my_inject { |sum, num| sum * num }
 end
-
-array = [1, 2, 3, 4]
-
-proc_argument = Proc.new { |number| number + 3 }
-
-p array.my_map_proc(proc_argument)
