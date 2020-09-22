c05ad6341389300990f64350376e21846e4da56a
unknown <martin_najjar@yahoo.com>
2020-09-22 18:11:40 +0300

    Add my_all, my_any and my_none methods

--- app.rb
+++ app.rb
@@ -1,38 +1,65 @@
+require 'pry'
+
 module Enumerable
   def my_each
-    # your code here
-   ary = self
-   for value in ary do
-    yield(value)
-   end
+    ary = self
+    for value in ary do
+      yield(value)
+    end
   end
 
   def my_each_with_index
     ary = self
-    for index in 0..ary.length-1 do
-      yield(ary[index],index)
+    for index in 0..ary.length - 1 do
+      yield(ary[index], index)
     end
   end
 
   def my_select
-    new_arr=[]
-    self.my_each do |element| 
+    new_arr = []
+    self.my_each do |element|
       if yield(element)
         new_arr << element
-      end  
+      end
     end
     new_arr
   end
+
+  def my_all?
+    boolean = true
+    self.my_each do |element|
+      unless yield(element)
+        boolean = false
+        break
+      end
+    end
+    boolean
+  end
+
+  def my_any?
+    boolean = false
+    self.my_each do |element|
+      binding.pry
+      if yield(element)
+        boolean = true
+        break
+      end
+    end
+    boolean
+  end
+
+  def my_none?
+    boolean = true
+    self.my_each do |element| 
+      if yield(element)
+        boolean = false
+        break
+      end
+    end
+    boolean
+  end
 end
 
-arr_nums = [1,2,3,3,5]
+arr_nums = [4,55,10,4,5]
 
-p arr_nums.my_select {|number| number == 3}
-
-# arr_nums.my_each_with_index do |value,index|
-#   puts "The index #{index} has the value #{value}"
-# end
-
-# arr_nums.my_each do |element|
-#   p element
-# end
+p arr_nums.my_none? { |number| number == 3 }
