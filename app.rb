cb40da88433e8d5f6663608a0741ba9f96389962
unknown <martin_najjar@yahoo.com>
2020-09-22 23:01:56 +0300

    Fix Linters' errors

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ module Enumerable
 
   def my_select
     new_arr = []
-    self.my_each do |element|
+    my_each do |element|
       yield(element) && new_arr << element
     end
     new_arr
@@ -22,7 +22,7 @@ module Enumerable
 
   def my_all?
     boolean = true
-    self.my_each do |element|
+    my_each do |element|
       unless yield(element)
         boolean = false
         break
@@ -33,7 +33,7 @@ module Enumerable
 
   def my_any?
     boolean = false
-    self.my_each do |element|
+    my_each do |element|
       if yield(element)
         boolean = true
         break
@@ -44,7 +44,7 @@ module Enumerable
 
   def my_none?
     boolean = true
-    self.my_each do |element|
+    my_each do |element|
       if yield(element)
         boolean = false
         break
@@ -56,11 +56,11 @@ module Enumerable
   def my_count(argument = nil)
     counter = 0
     if argument.nil?
-      self.my_each do |element|
+      my_each do |element|
         yield(element) && counter += 1
       end
     else
-      self.my_each do |element|
+      my_each do |element|
         argument == element && counter += 1
       end
     end
@@ -69,7 +69,7 @@ module Enumerable
 
   def my_map
     new_arr = []
-    self.my_each do |element|
+    my_each do |element|
       new_arr << yield(element)
     end
     new_arr
@@ -78,18 +78,17 @@ module Enumerable
   def my_inject(initial = nil)
     accum = 0
     if initial.nil?
-      initial = self[0]
-      self.my_each_with_index do |element, index|
-        break if index + 1 == self.length
-        if index.zero?
-          accum = yield(element, self[index + 1])
+      my_each_with_index do |element, index|
+        break if index + 1 == length
+
+        if index.zero? accum = yield(element, self[index + 1])
         else
           accum = yield(accum, self[index + 1])
         end
       end
     else
       accum = initial
-      self.my_each do |element|
+      my_each do |element|
         accum = yield(accum, element)
       end
     end
@@ -98,9 +97,8 @@ module Enumerable
 
   def my_map_proc(proc1 = nil)
     new_arr = []
-    self.my_each do |element|
-      if p1.nil?
-        new_arr << yield(element)
+    my_each do |element|
+      if proc1.nil? new_arr << yield(element)
       else
         new_arr << p1.call(element)
       end
@@ -112,9 +110,3 @@ end
 def multiply_els(array)
   array.my_inject { |sum, num| sum * num }
 end
-
-# array = [1, 2, 3, 4]
-
-# proc1 = Proc.new { |n| n * 2}
-
-# p array.my_map_proc(proc1)
