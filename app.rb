805ba060d3e7ec7f139d43be50fe425b70e5e2f0
unknown <martin_najjar@yahoo.com>
2020-09-22 20:16:20 +0300

    Modify my_inject and add multiply_els and my_map_proc

--- app.rb
+++ app.rb
@@ -77,11 +77,45 @@ module Enumerable
     new_arr
   end
 
-  def my_inject(initial=self[0])
-    accum = initial
-    self.my_each do |element|
-      accum = yield(accum,element)
+  def my_inject(initial = nil)
+    accum = 0
+    if initial === nil
+      initial = self[0]
+      self.my_each_with_index do |element, index|
+        if index + 1 == self.length
+          break
+        else
+          if index == 0
+            accum = yield(element, self[index + 1])
+          else
+            accum = yield(accum, self[index + 1])
+          end
+        end
+      end
+    else
+      accum = initial
+      self.my_each do |element|
+        accum = yield(accum, element)
     end
+  end
     accum
   end
+
+  def my_map_proc(p1)
+    new_arr = []
+    self.my_each do |element|
+      new_arr << p1.call(element)
+    end
+    new_arr
+  end
 end
+
+def multiply_els(array)
+  array.my_inject { |sum, num| sum * num }
+end
+
+array = [1, 2, 3, 4]
+
+proc_argument = Proc.new { |number| number + 3 }
+
+p array.my_map_proc(proc_argument)
