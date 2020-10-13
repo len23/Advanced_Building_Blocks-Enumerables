1b641ca951169e56341fa44c45a05abbf87fc3fa
martinnajjar12 <martin_najjar@yahoo.com>
2020-10-13 22:34:36 +0300

    Test my_none? method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -137,5 +137,53 @@ describe Enumerable do
       expect(falsy_array.my_any?(3)).to eql true
     end
   end
+
+  describe '#my_none?' do
+    it 'returns true if no argument is given, a block is given and none of the elements satisfy the condition' do
+      expect(array.my_none? { |n| n > 10 }).to eql true
+    end
+
+    it "returns false when no argument is given, a block is given and at least one element doesn't satisfy the condition" do
+      expect(array.my_none? { |n| n < 3 }).to eql false
+    end
+
+    it "returns false if no argument is given, no block is given and the array doesn't contain neither nil nor false" do
+      expect(array.my_none?).to eql false
+    end
+
+    it 'returns false if no argument is given, no block is given but at least one of the array elements is false' do
+      array << false
+      expect(array.my_none?).to eql false
+    end
+
+    it 'returns false if no argument is given, no block is given but at least one of the array elements is nil' do
+      array << nil
+      expect(array.my_none?).to eql false
+    end
+
+    it 'accepts an argument and returns true if none of the elements inside that array is equal to that argument' do
+      expect(falsy_array.my_none?(true)).to eql true
+    end
+
+    it 'returns true if the argument class is Regexp and none of the elements inside the array match that argument' do
+      expect(string_array.my_none?(/tie/)).to eql true
+    end
+
+    it 'returns true if the argument is Integer, Float, String, Hash or Array but none of the element classes inside that array match the argument' do
+      expect(string_array.my_none?(Integer)).to eql true
+    end
+
+    it 'returns true if the argument is Integer, Float, String, Hash or Array but none of the element classes inside that array match the argument' do
+      expect(hash_array.my_none?(Array)).to eql true
+    end
+
+    it 'returns true if the argument is Numeric but none of the element classes are neither Integer, Float nor Complex' do
+      expect(string_array.my_none?(Numeric)).to eql true
+    end
+
+    it 'returns true if the argument class is Integer, Float or String but none of the elements inside that array equal the argument' do
+      expect(threes_array.my_none?(4)).to eql true
+    end
+  end
 end
 # rubocop:enable Layout/LineLength
